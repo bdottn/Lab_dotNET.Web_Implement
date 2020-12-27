c43a22d0c15cfe5f083347e23edd6436b3bc07eb
Echo Lee <bdottn.lee+git@gmail.com>
2020-12-27 14:02:00 +0800

    專案（Web_Implement）ASP.NET Web API 建置
    1. 安裝 NuGet 套件：Microsoft.AspNet.WebApi（5.2.7）
    2. 加入靜態類別：WebApiConfig.cs，開啟屬性路由：調用 MapHttpAttributeRoutes
    3. 加入全域應用程式類別：Global.asax，在 Application_Start 配置屬性路由

---
+++ WebAPI_Implement/App_Start/WebApiConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace WebAPI_Implement
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // 屬性路由對應
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
