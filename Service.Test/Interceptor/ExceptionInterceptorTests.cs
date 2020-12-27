f9acb856f4827cac0a09a938ebd89785e7242fdd
Echo Lee <bdottn.lee+git@gmail.com>
2020-12-28 01:12:12 +0800

    例外攔截器（ExceptionInterceptor）實作：兼 Log 紀錄
    1. 安裝 NuGet 套件：Castle.Core（4.4.1）
    2. 安裝 NuGet 套件：log4net（2.0.12）
    3. 安裝 NuGet 套件：Newtonsoft.Json（12.0.3）
    4. 實作 ExceptionInterceptor 與測試案例：因為每次都執行都會經過攔截器，所以就順便作為 Service 的 Log 紀錄攔截器

---
+++ Service.Test/Interceptor/ExceptionInterceptorTests.cs
@@ -0,0 +1,77 @@
+﻿using Castle.DynamicProxy;
+using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Service.Interceptor;
+using Service.Model;
+using System;
+
+namespace Service.Test.Interceptor
+{
+    [TestClass]
+    public sealed class ExceptionInterceptorTests
+    {
+        private IInvocation invocation;
+
+        private ExceptionInterceptor interceptor;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.invocation = Substitute.For<IInvocation>();
+
+            this.interceptor = new ExceptionInterceptor();
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Service_ExceptionInterceptor_預期得到回傳值且呼叫過一次Proceed方法()
+        {
+            var expected = new ServiceResult()
+            {
+                ResultType = ServiceResultType.Success,
+                Message = "interceptor"
+            };
+
+            this.invocation.TargetType.Returns(typeof(IEmpty));
+            this.invocation.Method.Returns(typeof(IEmpty).GetMethod(nameof(IEmpty.Test)));
+            this.invocation.ReturnValue.Returns(expected);
+
+            this.interceptor.Intercept(this.invocation);
+
+            var actual = this.invocation.ReturnValue;
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+
+            this.invocation.Received(1).Proceed();
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Service_ExceptionInterceptor_呼叫Proceed方法發生例外_預期得到回傳值且呼叫過一次Proceed方法()
+        {
+            var expected = new ServiceResult()
+            {
+                ResultType = ServiceResultType.Exception,
+                Message = "exception"
+            };
+
+            this.invocation.TargetType.Returns(typeof(IEmpty));
+            this.invocation.Method.Returns(typeof(IEmpty).GetMethod(nameof(IEmpty.Test)));
+            this.invocation.When(i => i.Proceed()).Throw(new Exception(expected.Message));
+
+            this.interceptor.Intercept(this.invocation);
+
+            var actual = this.invocation.ReturnValue;
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+
+            this.invocation.Received(1).Proceed();
+        }
+
+        private interface IEmpty
+        {
+            ServiceResult Test();
+        }
+    }
+}
---
+++ Service/Interceptor/ExceptionInterceptor.cs
@@ -0,0 +1,111 @@
+﻿using Castle.DynamicProxy;
+using log4net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Service.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Service.Interceptor
+{
+    /// <summary>
+    /// 例外攔截器
+    /// </summary>
+    sealed class ExceptionInterceptor : IInterceptor
+    {
+        private readonly ILog log;
+
+        public ExceptionInterceptor()
+        {
+            this.log = LogManager.GetLogger(typeof(ExceptionInterceptor));
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            try
+            {
+                invocation.Proceed();
+
+                this.LogMessage(invocation, invocation.ReturnValue as ServiceResult);
+            }
+            catch (Exception ex)
+            {
+                var errorMessages = new List<string>();
+
+                while (ex != null)
+                {
+                    errorMessages.Add(ex.Message);
+                    ex = ex.InnerException;
+                }
+
+                var serviceResult = Activator.CreateInstance(invocation.Method.ReturnType) as ServiceResult;
+
+                serviceResult.ResultType = ServiceResultType.Exception;
+                serviceResult.Message = string.Join("。", errorMessages);
+
+                invocation.ReturnValue = serviceResult;
+
+                this.LogMessage(invocation, serviceResult);
+            }
+        }
+
+        private void LogMessage(IInvocation invocation, ServiceResult serviceResult)
+        {
+            var messages = new List<string>()
+            {
+                "Service",
+                $"{invocation.TargetType.Name}.{invocation.Method.Name}",
+                serviceResult.ResultType == ServiceResultType.Success ? "成功": serviceResult.Message,
+            };
+
+            // Log 層級設在 DEBUG 或 ALL 時，記錄參數。Log 層級設在 INFO 以上時，非 Success 的結果才記錄參數
+            if (this.log.IsDebugEnabled || serviceResult.ResultType != ServiceResultType.Success)
+            {
+                messages.Add(FormatArgs(invocation));
+            }
+
+            // Service | MethodType.MethodName | ServiceMessage | Arg
+            var message = string.Join(" | ", messages);
+
+            switch (serviceResult.ResultType)
+            {
+                case ServiceResultType.Success:
+                    this.log.Info(message);
+                    break;
+
+                case ServiceResultType.Warning:
+                    this.log.Warn(message);
+                    break;
+
+                case ServiceResultType.Fail:
+                    this.log.Error(message);
+                    break;
+
+                case ServiceResultType.Exception:
+                    this.log.Fatal(message);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static string FormatArgs(IInvocation invocation)
+        {
+            if (invocation.Arguments == null || invocation.Arguments.Length == 0)
+            {
+                return null;
+            }
+
+            var args = new Dictionary<string, object>();
+            var methodParams = invocation.Method.GetParameters();
+
+            for (int idx = 0; idx < invocation.Arguments.Length; idx++)
+            {
+                args.Add(methodParams[idx].Name, invocation.Arguments[idx]);
+            }
+
+            return JsonConvert.SerializeObject(args, new StringEnumConverter());
+        }
+    }
+}
