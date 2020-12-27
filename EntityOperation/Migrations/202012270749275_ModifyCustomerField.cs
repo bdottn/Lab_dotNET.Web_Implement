180b59c63c1f15e286d537ca96f7156ef1a5ead4
Echo Lee <bdottn.lee+git@gmail.com>
2020-12-27 15:53:31 +0800

    專案資料庫建置
    1. Customer 新增欄位，並掛上驗證屬性
    2. 執行指令（Add-Migration）產生 Migration 更新文件
    3. 執行指令（Update-Database -Script）產生 TSQL 更新指令，並至資料庫執行
    4. 儲存 Migration 更新文件、TSQL 更新指令至 Migrations 資料夾

---
+++ EntityOperation/Migrations/202012270749275_ModifyCustomerField.cs
@@ -0,0 +1,23 @@
+﻿namespace EntityOperation.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class ModifyCustomerField : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Phone", c => c.String(maxLength: 24));
+            AddColumn("dbo.Customers", "CreatedTime", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Customers", "LatestModifiedTime", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 30));
+        }
+
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+            DropColumn("dbo.Customers", "LatestModifiedTime");
+            DropColumn("dbo.Customers", "CreatedTime");
+            DropColumn("dbo.Customers", "Phone");
+        }
+    }
+}
--- EntityOperation/Model/Customer.cs
+++ EntityOperation/Model/Customer.cs
@@ -1,4 +1,7 @@
-﻿namespace EntityOperation.Model
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityOperation.Model
 {
     /// <summary>
     /// 客戶
@@ -8,11 +11,33 @@
         /// <summary>
         /// 客戶序號
         /// </summary>
+        [Required]
+        [Key]
         public int Id { get; set; }
 
         /// <summary>
         /// 客戶名稱
         /// </summary>
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
+
+        /// <summary>
+        /// 客戶電話
+        /// </summary>
+        [MaxLength(24)]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        [Required]
+        public DateTime CreatedTime { get; set; }
+
+        /// <summary>
+        /// 最後更新時間
+        /// </summary>
+        [Required]
+        public DateTime LatestModifiedTime { get; set; }
     }
 }
