78866b54e2394ed7370cd83ae8e1fbaf1ca8acc6
Echo Lee <bdottn.lee+git@gmail.com>
2020-12-27 12:31:03 +0800

    ORM 操作處理
    1. 建立 ISQLRepository 泛型介面
    2. 實作 SQLRepository 泛型類別

---
+++ EntityOperation/Protocol/ISQLRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace EntityOperation.Protocol
+{
+    /// <summary>
+    /// 資料庫物件操作介面
+    /// </summary>
+    /// <typeparam name="TModel">資料庫物件型別</typeparam>
+    public interface ISQLRepository<TModel>
+        where TModel : class
+    {
+        /// <summary>
+        /// 建立資料庫物件
+        /// </summary>
+        /// <param name="model">資料庫物件</param>
+        void Create(TModel model);
+    }
+}
---
+++ EntityOperation/SQLRepository.cs
@@ -0,0 +1,18 @@
+﻿using EntityOperation.Protocol;
+
+namespace EntityOperation
+{
+    sealed class SQLRepository<TModel>
+        : ISQLRepository<TModel>
+        where TModel : class
+    {
+        public void Create(TModel model)
+        {
+            using (var context = new LabContext())
+            {
+                context.Set<TModel>().Add(model);
+                context.SaveChanges();
+            }
+        }
+    }
+}
